120468adf6e2954042cbea0b5f82f68ae3a445ca
Hyungki Kim <diskhkme@gmail.com>
2019-02-04 17:13:05 +0900

    5 ListView 추가, Read Notebook & Readnote 구현
    ListView 추가하여 Notebook과 Note와 바인딩.
    ReadNote(book) 함수를 통해 DB에서 테이블을 읽어오고, 이를 NoteVM 생성자에서 호출
    ReadNote에서는 Lamda Expression을 사용하여 SelectedNotebook의 ID와 일치하는 Note만 불러옴

--- NotesApp/ViewModel/NotesVM.cs
+++ NotesApp/ViewModel/NotesVM.cs
@@ -1,5 +1,6 @@
 ﻿using NotesApp.Model;
 using NotesApp.ViewModel.Commands;
+using SQLite;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -33,6 +34,11 @@ namespace NotesApp.ViewModel
         {
             NewNotebookCommand = new NewNotebookCommand(this);
             NewNoteCommand = new NewNoteCommand(this);
+
+            Notebooks = new ObservableCollection<Notebook>();
+            Notes = new ObservableCollection<Note>();
+
+            ReadNotebooks();
         }
 
         public void CreateNotebook()
@@ -56,6 +62,38 @@ namespace NotesApp.ViewModel
             };
 
             DatabaseHelper.Insert(newNote);
+        }
+
+        public void ReadNotebooks()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(DatabaseHelper.dbFile))
+            {
+                var notebooks = conn.Table<Notebook>().ToList();
+
+                Notebooks.Clear();
+                foreach(var notebook in notebooks)
+                {
+                    Notebooks.Add(notebook);
+                }
+            }
+        }
+
+        public void ReadNotes()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(DatabaseHelper.dbFile))
+            {
+                if(SelectedNotebook != null)
+                {
+                    //Notebook ID에 해당하는 Note만 불러옴
+                    var notes = conn.Table<Note>().Where(n => n.NotebookId == SelectedNotebook.Id).ToList();
+
+                    Notes.Clear();
+                    foreach (var note in notes)
+                    {
+                        Notes.Add(note);
+                    }
+                }
+            }
         }
     }
 }

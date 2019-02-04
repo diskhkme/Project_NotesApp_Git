18cabdfe399062021604b0ee1385938771994e44
Hyungki Kim <diskhkme@gmail.com>
2019-02-04 15:34:26 +0900

    3 전반 구조 구현
    LoginVM >> 1) User 데이터 포함
                          2) LoginCommand와 RegisterCommand 관리, Command들은 ICommand 구현.
    NotesVM >> 1) Notebook 및 Note 데이터 포함 (ObservableCollection 타입)
                           2) NewNotebookCommand와 NewNoteCommand 관리
    LoginWindow, NotesWindow 생성

---
+++ NotesApp/ViewModel/Commands/NewNoteCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NotesApp.ViewModel.Commands
+{
+    public class NewNoteCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        public NotesVM VM { get; set; }
+
+        public NewNoteCommand(NotesVM vm)
+        {
+            VM = vm;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            //TODO : 새 노트 생성
+        }
+    }
+}

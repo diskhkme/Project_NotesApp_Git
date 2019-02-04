76ea16c6d7518dcaf2dc27117386c44726fd47a7
Hyungki Kim <diskhkme@gmail.com>
2019-02-04 15:09:01 +0900

    1 User, Notebook, Note.cs 구현
    모델, INotifyPropertyChanged 인터페이스 구현

---
+++ NotesApp/Model/Note.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.Model
+{
+    //Note information
+    public class Note : INotifyPropertyChanged
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        private int notebookId;
+        //자신이 속한 notebook의 ID
+        public int NotebookId
+        {
+            get { return notebookId; }
+            set {
+                notebookId = value;
+                OnPropertyChanged("NotebookId");
+            }
+        }
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set {
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+        private DateTime updatedTime;
+
+        public DateTime UpdatedTime
+        {
+            get { return updatedTime; }
+            set {
+                updatedTime = value;
+                OnPropertyChanged("UpdatedTime");
+            }
+        }
+
+        private string fileLocation;
+
+        public string FileLocation
+        {
+            get { return fileLocation; }
+            set {
+                fileLocation = value;
+                OnPropertyChanged("FileLocation");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
---
+++ NotesApp/Model/Notebook.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.Model
+{
+    //Notebook Information(노트의 집합)
+    public class Notebook : INotifyPropertyChanged
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        private int userId;
+        //자신이 속한 User의 ID
+        public int UserId
+        {
+            get { return userId; }
+            set {
+                userId = value;
+                OnPropertyChanged("UserId");
+            }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}

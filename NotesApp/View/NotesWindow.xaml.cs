f3e9b480a09c09f20ec079a4925e9f9160dd2d3e
Hyungki Kim <diskhkme@gmail.com>
2019-02-04 17:49:33 +0900

    7 SpeechRecognizer
    현재 환경에서 에러가 나서 테스트 불가능. Optional Content로 생각

--- NotesApp/View/NotesWindow.xaml.cs
+++ NotesApp/View/NotesWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Speech.Recognition;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,19 +21,44 @@ namespace NotesApp.View
     /// </summary>
     public partial class NotesWindow : Window
     {
+        SpeechRecognitionEngine recognizer;
+        bool isRecognizing = false;
+
         public NotesWindow()
         {
             InitializeComponent();
+
+            //참조 --> 참조 추가 --> System.Speech
+            //내 시스템 culture를 가져옴
+            var currentCulture = (from r in SpeechRecognitionEngine.InstalledRecognizers()
+                                 where r.Culture.Equals(Thread.CurrentThread.CurrentCulture)
+                                 select r).FirstOrDefault();
+            //내 시스템에서는 recognizer가 null이라서 오류 발생..
+            if(recognizer != null)
+            {
+                recognizer = new SpeechRecognitionEngine(currentCulture);
+
+                GrammarBuilder builder = new GrammarBuilder();
+                builder.AppendDictation();
+                Grammar grammar = new Grammar(builder);
+
+                recognizer.LoadGrammar(grammar);
+                recognizer.SetInputToDefaultAudioDevice();
+                recognizer.SpeechRecognized += Recofnizer_SpeechRecognized;
+            }
+            
+        }
+
+        private void Recofnizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string recognizedText = e.Result.Text;
+
+            contentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(recognizedText)));
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
 
         private void contentRichTextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -44,6 +71,20 @@ namespace NotesApp.View
         private void boldButton_Click(object sender, RoutedEventArgs e)
         {
             contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Bold);
+        }
+
+        private void speechButtonClick(object sender, RoutedEventArgs e)
+        {
+            if(!isRecognizing)
+            {
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+                isRecognizing = true;
+            }
+            else
+            {
+                recognizer.RecognizeAsyncStop();
+                isRecognizing = false;
+            }
         }
     }
 }
